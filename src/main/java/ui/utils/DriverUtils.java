591257a9889d89e494e0cdc51646b8be6f2b04e1
Cigdem <cigdemrespect@gmail.com>
2022-01-26 15:27:37 -0600

    Login.feature

--- src/main/java/ui/utils/DriverUtils.java
+++ src/main/java/ui/utils/DriverUtils.java
@@ -1,10 +1,17 @@
 package ui.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class DriverUtils {
@@ -15,15 +22,40 @@ public class DriverUtils {
 
     public static WebDriver getDriver(){
         String browser = ConfigReader.readProperty("browser");
+        String remoteUrl = "http://54.172.206.147:4444";
         if (driver == null){
             switch (browser){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
+                case "chromeremote":
+                    File chromeFile = new File("C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
+                    DesiredCapabilities chromeCapabilities = DesiredCapabilities.chrome();
+                    chromeCapabilities.setBrowserName("chrome");
+                    chromeCapabilities.setPlatform(Platform.ANY);
+                    try {
+                        driver = new RemoteWebDriver(new URL(remoteUrl), chromeCapabilities);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
+                case "firefoxremote":
+                    File firefoxFile = new File("C:\\Users\\Administrator\\Downloads\\geckodriver.exe");
+                    System.setProperty("webdriver.gecko.driver", firefoxFile.getAbsolutePath());
+                    DesiredCapabilities firefoxCapabilities = DesiredCapabilities.firefox();
+                    firefoxCapabilities.setBrowserName("firefox");
+                    firefoxCapabilities.setPlatform(Platform.ANY);
+                    try {
+                        driver = new RemoteWebDriver(new URL(remoteUrl), firefoxCapabilities);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     WebDriverManager.chromedriver().setup();
@@ -32,7 +64,6 @@ public class DriverUtils {
             }
             driver.manage().window().maximize();
             driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver;
     }
